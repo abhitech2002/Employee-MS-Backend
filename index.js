74c1dbd0eb722c6d79f696528f95f7b1d36dbf28
abhitech2002 <oabhishek31@gmail.com>
2024-05-14 18:20:58 +0530

    Data Base Conneted

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
-app.listen(3000, (req, res) => {
-    console.log("Server Started on server 3000....")
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose
+    .connect(MONGODB_URI)
+    .then( () => {
+        console.log("MongoDB connected")
+    })
+    .catch((error) => console.log(error))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server Started on server ${PORT}....`)
 })
